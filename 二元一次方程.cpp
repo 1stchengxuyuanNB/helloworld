e8636fcad7959562020e42ace9b9e8d70b9c7a6f
1stchengxuyuanNB <32224037+1stchengxuyuanNB@users.noreply.github.com>
2017-10-09 22:27:21 +0800

    Add files via upload

---
+++ 二元一次方程.cpp
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main ()
+{
+	float a,b,c,d,e,f,x,y;
+	printf("这是一个解二元一次方程组的程序。\n请输入六个数字分别为（a,b,c,d,e,f）.\n");
+	printf("满足\nax+by=c \ndx+ey=f\n");
+	scanf("%f %f %f %f %f %f",&a,&b,&c,&d,&e,&f );
+	x=(c*e-b*f)/(a*e-b*d);
+	printf("x=%f.\n",x);
+	y=(c*d-a*f)/(b*d-a*e);
+	printf("y=%f.\n",y);
+	return 0;
+}
+
