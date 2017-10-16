bb3f53a392fde48d29aaaaaa5ea282d1fa2038ce
1stchengxuyuanNB <32224037+1stchengxuyuanNB@users.noreply.github.com>
2017-10-16 22:09:39 +0800

    Add files via upload

---
+++ 10.10.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x=2.2;
+	printf("%d\n",x);
+	short y=0x7FFF;
+	printf("%d\n",y);
+	short z=0xFFF;
+	printf("%d\n",z);
+	unsigned short w=0xFFFF;
+	printf("%d\n",w);
+	float u=0.177;
+	printf("%f\n",u);
+	
+
+ } 
